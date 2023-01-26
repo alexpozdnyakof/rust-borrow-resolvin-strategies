ac3345108f995bf15c57597f55a0740111fdb7a7
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-01-26 17:00:38 +0700

    add refcell

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
-struct GroundStation {}
+struct GroundStation {
+    radio_freq: f64
+}
 
 fn main() {
-    let base = Rc::new(GroundStation {});
+    let base: Rc<RefCell<GroundStation>> = Rc::new(
+        RefCell::new(
+            GroundStation {
+                radio_freq: 87.65
+            }
+        )
+    );
 
-    println!("{:?}", base);
+    println!("base {:?}", base);
+
+    {
+        let mut base_2 = base.borrow_mut();
+        base_2.radio_freq -= 12.34;
+        println!("base_2: {:?}", base_2);
+    }
+
+    println!("base: {:?}", base);
+
+    let mut base_3 = base.borrow_mut();
+    base_3.radio_freq += 43.21;
+
+    println!("base: {:?}", base);
+    println!("base_3: {:?}", base_3);
 }
